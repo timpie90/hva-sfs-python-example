43d376971f8cbc1543e5cea11eed9f87a2b13e1b
Tim <tim-vlaar@hotmail.com>
2019-09-10 09:17:22 +0200

    some calculations

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from api.amsterdam_api import AmsterdamApi
+import statistics
 
 
 def main():
     amsterdam_api = AmsterdamApi()
     list_trash_bins = amsterdam_api.get_trash_bins()
+    list_monuments = amsterdam_api.get_monuments()
 
     print("Overview of trash bins in Amsterdam")
 
@@ -13,6 +15,17 @@ def main():
             trash_bin['name'] + "\t" +
             trash_bin['type'] + "\t" +
             trash_bin['address']
+        )
+
+    print("\nTotal amount of trash bins : ", list_trash_bins.__len__())
+    print("Total amount of Papier trash bins : ", sum(trash_bin['type'] == 'Papier' for trash_bin in list_trash_bins))
+    print("Total amount of Rest trash bins : ", sum(trash_bin['type'] == 'Rest' for trash_bin in list_trash_bins))
+    print("Average id number : ", statistics.mean(trash_bin['id'] for trash_bin in list_trash_bins), "\n")
+
+    for monument in list_monuments:
+        print(
+            str(monument['id']) + "\t" +
+            monument['address']
         )
 
 
